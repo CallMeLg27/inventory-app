3258b1de320956648ca403ca9e4b79af3765e12c
itsgerman27 <lgeldragondorado@gmail.com>
2021-11-04 00:16:15 -0500

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PriceDisplayComponent } from './price-display/price-display.component';
+import { ProductDepartmentComponent } from './product-department/product-department.component';
+import { ProductImageComponent } from './product-image/product-image.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PriceDisplayComponent,
+    ProductDepartmentComponent,
+    ProductImageComponent,
+    ProductRowComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
