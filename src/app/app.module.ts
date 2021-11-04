e6bfb0bdce10825e2fdbc27ad9365463ae0375db
itsgerman27 <lgeldragondorado@gmail.com>
2021-11-04 02:29:15 -0500

    finish implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { PriceDisplayComponent } from './price-display/price-display.component';
 import { ProductDepartmentComponent } from './product-department/product-department.component';
 import { ProductImageComponent } from './product-image/product-image.component';
 import { ProductRowComponent } from './product-row/product-row.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductsListComponent as ProductsListComponent } from './products-list/products-list.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +16,11 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductDepartmentComponent,
     ProductImageComponent,
     ProductRowComponent,
-    ProductListComponent
+    ProductsListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
